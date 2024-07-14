9adfeccb385402cc27dd43520a109eea4f64b3c4
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-07-14 10:13:18 +0530

    server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://manage-ur-inventory.netlify.app/"],
+    origin: ["http://localhost:3000", "https://manage-ur-inventory.netlify.app"],
     credentials: true,
   })
 );
