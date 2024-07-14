2ce2308d14bf7843952ff272bf88016251f9d478
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-07-14 09:20:52 +0530

    server.js

--- server.js
+++ server.js
@@ -24,12 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000","https://manage-ur-inventory.netlify.app/"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
