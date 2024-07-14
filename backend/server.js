d9818149d643abf06e12253838a21baa10fc597a
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-07-14 08:55:46 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://inventory-management-6ael.onrender.com"],
     credentials: true,
   })
 );
