99009a11c73663f8d9d2bd0617779920b7961487
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-07-14 09:29:38 +0530

    server.js

--- server.js
+++ server.js
@@ -28,14 +28,6 @@ app.use(cors({
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://manage-ur-inventory.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
